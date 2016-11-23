9e572b72c5e1e3c6224cbf8b93ce85bf7ef1409b
David Fung <github@fungservices.com>
2016-11-23 13:50:28 -0800

    Added links

---
+++ david.R
@@ -0,0 +1,4 @@
+prezdata <- read.csv("~/SpiderOak Hive/UC Davis/STA 141A/Final Project/datasets/president_general_polls_2016.csv")
+head(prezdata, 10)
+levels(prezdata$pollster)
+levels(prezdata$state)
