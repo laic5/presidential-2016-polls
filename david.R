4bca25dc744f8e611fd47d2d37e12b692407eeb7
David Fung <github@fungservices.com>
2016-11-23 13:51:09 -0800

    Delete david.R

--- david.R
+++
@@ -1,4 +0,0 @@
-prezdata <- read.csv("~/SpiderOak Hive/UC Davis/STA 141A/Final Project/datasets/president_general_polls_2016.csv")
-head(prezdata, 10)
-levels(prezdata$pollster)
-levels(prezdata$state)
