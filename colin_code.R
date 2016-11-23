42d9d4a71b73931d44f3c0c24d62199b3bebda48
sssantos <colinsantos@Colins-MacBook-Pro.local>
2016-11-23 11:22:50 -0800

    Colin's code
    Datasets subsetted into raw and the adjusted types. Some exploratory
    scratch.

---
+++ colin_code.R
@@ -0,0 +1,71 @@
+# STA141A Project Code
+# Colin P. Santos 
+# 11.20.16
+
+##################################################
+data <- read.csv("project_data.csv")
+attach(data)
+
+# Here are the dataframes sorted by adjustment types and raw
+polls_plus <- subset(data, type == "polls-plus", select = -c(type, rawpoll_clinton, rawpoll_johnson, rawpoll_mcmullin, rawpoll_trump))
+polls_only <- subset(data, type == "polls-only", select = -c(type, rawpoll_clinton, rawpoll_johnson, rawpoll_mcmullin, rawpoll_trump))
+now_cast   <- subset(data, type == "now-cast"  , select = -c(type, rawpoll_clinton, rawpoll_johnson, rawpoll_mcmullin, rawpoll_trump))
+raw_polls  <- subset(data, select = -c(type, adjpoll_clinton, adjpoll_johnson, adjpoll_mcmullin, adjpoll_trump))[1:(nrow(data)/length(levels(type))),]
+
+##################################################
+
+# Exploratory Scratch
+
+state[14]
+state[19]
+astate[8]
+state[9] #New Mexico
+state[10]
+state[151] #New Mexico
+which(state == "California")
+which(state == "New Mexico")
+which(state == levels(state)[5])
+which(state == levels(state)[37])
+levels(state)[37]
+
+
+nrow(data)
+head
+
+attach(data)
+levels(branch)
+levels(as.factor(cycle))
+levels(type)
+levels(matchup)
+levels(forecastdate)
+levels(grade)
+ncol(data)
+levels(multiversions)
+levels(timestamp)
+length(grade)
+levels(pollster)
+levels(state)
+length(levels(state)) #57 "state"s
+us_congressio
+library(USAboundaries)
+plot(us_congressional())
+va_congressional <- us_congressional(states="california")
+if (require(sp)) {
+  plot(va_congressional)
+}
+
+
+head(data)
+data[9,]
+data$state[9]
+data$state
+as.character(data$state)
+table(as.character(data$state))
+sort(table(as.character(data$state)))
+library(lattice)
+?lattice
+xyplot(sort(table(as.character(data$state))))
+xyplot(y=sort(table(as.character(data$state))))
+plot(sort(table(as.character(data$state))))
+
+##################################################
