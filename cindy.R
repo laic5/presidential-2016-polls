14fffdbd6aa8dce08ec2f46574d5d55848aa6c16
laic5 <cynthialai1996@gmail.com>
2016-11-23 14:05:39 -0800

    Comparing Trump and Clinton %. still need to fix NaN values

--- cindy.R
+++ cindy.R
@@ -1,10 +1,8 @@
 setwd("C:/Users/Cindy/Documents/Davis/JUNIOR/STA 141A/project")
 
 library(tidyverse)
-<<<<<<< HEAD
 library(plyr)
-=======
->>>>>>> b7f3089e98b2a67ff9d0002e98b878015ea90468
+
 
 dat = read.csv("president_general_polls_2016.csv")
 summary(dat)
@@ -35,7 +33,8 @@ summary(florida$grade)
 # close up to after August
 recent = subset(florida, startdate > "2016-08-08")
 
-<<<<<<< HEAD
+ggplot(recent, aes(startdate, adjpoll_clinton)) + geom_line(color = "blue") + geom_line(aes(startdate, adjpoll_trump), color = "red") + facet_grid(grade ~ .)
+
 ggplot(recent, aes(startdate, adjpoll_clinton)) + geom_line(color = "blue") + geom_line(aes(startdate, adjpoll_trump), color = "red") + facet_grid(grade ~ .)
 
 
@@ -45,24 +44,36 @@ sum(florida$poll_wt * florida$rawpoll_trump/100) # notice how Trump's is smaller
 
 good = dat[which(dat$grade == c("A", "A+", "A-")),]
 
-iowa = good[which(good$state == "Iowa"),]
-plus = iowa[which(iowa$type == "polls-plus"),]
-mean(plus$adjpoll_clinton)
-
 states.list = as.vector(unique(dat$state))
+iowa = good[which(dat$state == "Maine CD-1"),]
+only =  iowa[which(iowa$type == "polls-only"),]
 
-state.mean = ldply(states.list, function(stat) {
+
+
+state.mean.clinton = ldply(states.list, function(stat) {
   state.data = good[which(good$state == stat),]
   
   plus = state.data[which(state.data$type == "polls-plus"),]
   now = state.data[which(state.data$type == "now-cast"),]
   only = plus = state.data[which(state.data$type == "polls-only"),]
   
-  c(stat, mean(plus$adjpoll_clinton), mean(now$adjpoll_clinton), mean(only$adjpoll_clinton))
+  c(stat, mean(plus$adjpoll_clinton, na.rm = T), mean(now$adjpoll_clinton, na.rm = T), mean(only$adjpoll_clinton, na.rm = T))
   
   })
 
 state.mean[which(state.mean$V2 >= 50 | state.mean$V3 >= 50 | state.mean $V4 >= 50),1]
-=======
-ggplot(recent, aes(startdate, adjpoll_clinton)) + geom_line(color = "blue") + geom_line(aes(startdate, adjpoll_trump), color = "red") + facet_grid(grade ~ .)
->>>>>>> b7f3089e98b2a67ff9d0002e98b878015ea90468
+
+
+state.mean.trump = ldply(states.list, function(stat) {
+  state.data = good[which(good$state == stat),]
+  
+  plus = state.data[which(state.data$type == "polls-plus"),]
+  now = state.data[which(state.data$type == "now-cast"),]
+  only = plus = state.data[which(state.data$type == "polls-only"),]
+  
+  c(stat, mean(plus$adjpoll_trump, na.rm = T), mean(now$adjpoll_trump, na.rm = T), mean(only$adjpoll_trump, na.rm = T))
+  
+})
+
+
+state.mean[which(state.mean.clinton$V2 >= state.mean.trump$V2 | state.mean.clinton$V3 >= state.mean.trump$V3 | state.mean.clinton$V4 >= state.mean.trump$V4),1]
